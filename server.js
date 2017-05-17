60b088058f4dfd9ebd22776d51573f571fc23132
WillyWunderdog <odemichaelgbenga@gmail.com>
2017-05-17 15:40:10 +0100

    update for heroku

---
+++ server.js
@@ -0,0 +1,30 @@
+//import dependencies
+
+const express = require('express'),
+bodyParser = require('body-parser'),
+router = express.Router(),
+dotenv = require('dotenv').config(),
+auth = require('./controllers/auth.js');
+
+//initialize express
+const app = express();
+
+var port = process.env.PORT || 8000;
+
+//set up view engine
+app.use('/public',express.static(process.cwd() + '/public'));
+app.set('view engine', 'ejs');
+
+
+
+
+
+// grab form data 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(require('../routes/events.js'));
+
+app.listen(port, () => {
+	console.log("Now listening on  " + port)}
+)
